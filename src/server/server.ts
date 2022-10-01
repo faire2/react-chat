86b82edbc5a1750485732a59f32e79bb36acfadd
faire <faire.fionwe@gmail.com>
2022-10-01 16:34:03 +0200

    Basic Socket IO implementation on server

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,28 @@
 import cors from "cors"
 import express from "express"
 import http from "http"
+import { Server } from "socket.io"
 
 const app = express()
 app.use(cors())
 
 const port = process.env.PORT || 4001
 const server = http.createServer(app)
+
+const io = new Server(server, {
+	cors: {
+		origin: 'http://localhost:3000',
+		methods: ["GET", "POST"]
+	}
+})
+
+io.on('connection', (socket) => {
+	console.log(`Logged  in: ${socket.id}`)
+
+	socket.on('disconnect', () => {
+		console.log(`Logged out: ${socket.id}`)
+	})
+})
 
 app.get('/api', (req, res) => {
 	res.json({
