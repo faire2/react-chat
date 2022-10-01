e8d5e1486768b22edbdfd6f786b6797550e0e5a2
faire <faire.fionwe@gmail.com>
2022-10-01 17:24:48 +0200

    Transmission types

--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,10 @@ export enum Transmissions {
 	NewMessages = "NewMessages",
 	SendNewMessage = "SendNewMessage"
 }
+
+
+export interface Message {
+	message: string
+	userId: string
+}
+export type NewMessages = Message[]
