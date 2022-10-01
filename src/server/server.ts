2f79bf4050da6673c92947da1e8af776a380adc0
faire <faire.fionwe@gmail.com>
2022-10-01 16:27:40 +0200

    Add cors and http

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,12 @@
+import cors from "cors"
 import express from "express"
+import http from "http"
 
 const app = express()
+app.use(cors())
+
 const port = process.env.PORT || 4001
+const server = http.createServer(app)
 
 app.get('/api', (req, res) => {
 	res.json({
@@ -9,6 +14,6 @@ app.get('/api', (req, res) => {
 	})
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
 	console.log(`Server listening on ${port}`)
 })
