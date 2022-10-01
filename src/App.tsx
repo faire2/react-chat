5607b738302d88abb974532d86124196ed6db97e
faire <faire.fionwe@gmail.com>
2022-10-01 17:05:02 +0200

    Add chatroom to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 import io from 'socket.io-client'
 
+import Chatroom from './Chatroom'
+
 const socket = io('http://localhost:4001')
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello chat app!
+        <Chatroom socket={socket} />
       </header>
     </div>
   );
